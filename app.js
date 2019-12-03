c7bd873c292f7e915daca89832919f1a151b760e
Tony <bootcamp1955@gmail.com>
2019-12-03 14:25:19 -0500

    index.html

---
+++ app.js
@@ -0,0 +1,9 @@
+const animals = document.querySelectorAll('.animal');
+console.log(animals);
+
+// for(let i = 0; i < animals.length; i++) {
+//     animals[i].addEventListener('click', function() {
+//         let animal = this.innerHTML;
+//         console.log(animal);
+//     })
+// }
